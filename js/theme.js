ea11f61e03bcfc72e2d49b3014e476b78ab583b9
Hüseyin Bal <34191951+huseyinbal05@users.noreply.github.com>
2025-12-30 17:18:16 +0300

    Delete js directory

--- js/theme.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    try {
-        const savedTheme = localStorage.getItem('aybu_theme_v1');
-        const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        if (savedTheme === 'dark' || (!savedTheme && systemDark)) {
-            document.documentElement.classList.add('dark');
-        }
-    } catch(e){}
-})();
